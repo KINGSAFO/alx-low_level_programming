4837627d6b6ea1aaa6ef8ae99d7d2bbb4a182487
KingSafo <kofisafo26@yahoo.com>
2022-10-21 17:00:02 -0700

    Two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**print_line - prints a straight line using '_'
+/**
+ * print_line - a function that draws a straight line using '_'
  * @n: the number of '_' to be printed
  */
 void print_line(int n)
